8aef4a0a17ab95efeea5204777087c4c8564c942
1298786483 <1298786483@qq.com>
2025-11-26 23:30:22 +0800

    Site updated: 2025-11-26 23:30:21

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -1,27 +1,42 @@
-var a_idx = 0;
-jQuery(document).ready(function($) {
-    $("body").click(function(e) {
+/* 鼠标点击特效 - 核心价值观 */
+(function() {
+    var a_idx = 0;
+    window.onclick = function(event) {
         var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
-        var $i = $("<span/>").text(a[a_idx]);
+        
+        var heart = document.createElement("b"); // 创建 b 元素
+        heart.onselectstart = new Function('event.returnValue=false'); // 防止选中
+        
+        document.body.appendChild(heart).innerHTML = a[a_idx]; // 将词语写入
         a_idx = (a_idx + 1) % a.length;
-        var x = e.pageX,
-            y = e.pageY;
-        $i.css({
-            "z-index": 99999,
-            "top": y - 20,
-            "left": x,
-            "position": "absolute",
-            "font-weight": "bold",
-            "color": "#ff6651", // 这里可以修改字体颜色
-            "user-select": "none",
-            "cursor": "default"
-        });
-        $("body").append($i);
-        $i.animate({
-            "top": y - 180,
-            "opacity": 0
-        }, 1500, function() {
-            $i.remove();
-        });
-    });
-});
+        
+        heart.style.cssText = "position: fixed;left:-100%;"; // 初始化位置
+        
+        var f = 16, // 字体大小
+            x = event.clientX - f / 2, // 横坐标
+            y = event.clientY - f, // 纵坐标
+            c = randomColor(), // 随机颜色
+            a = 1, // 透明度
+            s = 1.2; // 放大倍数
+            
+        var timer = setInterval(function() { // 定时器
+            if (a <= 0) {
+                document.body.removeChild(heart);
+                clearInterval(timer);
+            } else {
+                heart.style.cssText = "font-size:16px;cursor: default;position: fixed;color:" +
+                    c + ";left:" + x + "px;top:" + y + "px;opacity:" + a + ";transform:scale(" +
+                    s + ");";
+                    
+                y--;
+                a -= 0.016;
+                s += 0.002;
+            }
+        }, 15);
+    }
+    
+    // 随机颜色函数
+    function randomColor() {
+        return "rgb(" + (~~(Math.random() * 255)) + "," + (~~(Math.random() * 255)) + "," + (~~(Math.random() * 255)) + ")";
+    }
+}());
