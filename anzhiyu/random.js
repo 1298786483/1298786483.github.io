25668208a99041e9643fd1feda5023ee612c7971
1298786483 <1298786483@qq.com>
2025-11-26 18:35:42 +0800

    Site updated: 2025-11-26 18:35:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/19/2025-11-19-hexo-anzhiyu-setup-detailed/","2025/11/21/2025-11-21-neu-booking-bot/","2025/11/19/我的个人博客/","2025/11/18/hello-world/"];function toRandomPost(){
+var posts=["2025/11/19/2025-11-19-hexo-anzhiyu-setup-detailed/","2025/11/19/我的个人博客/","2025/11/21/2025-11-21-neu-booking-bot/","2025/11/18/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
